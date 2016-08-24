eb988dc1956ccb8f6d823081dab19ab78acedc44
andrey1s <andrey@4devs.pro>
2016-08-24 11:20:38 +0300

    phpcs

--- Filesystem/DownloadInterface.php
+++ Filesystem/DownloadInterface.php
@@ -4,5 +4,10 @@ namespace FDevs\Backup\Filesystem;
 
 interface DownloadInterface
 {
+    /**
+     * @param string $target
+     *
+     * @return string
+     */
     public function download($target);
 }
